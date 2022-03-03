9cf89b0a32509ac0bca544507d19175be2853d76
Hafizur Rahman <hafizur.rahman@STS.local>
2022-03-03 10:54:31 -0500

    host change

--- producer/app.py
+++ producer/app.py
@@ -13,7 +13,7 @@ async def root():
 def post_message(message: str):
 
     connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host="rabbit@rabbitmq-0.rabbitmq-headless.keda.svc.cluster.local", port=5672, 
+    pika.ConnectionParameters(host="10.0.166.131", port=5672, 
     credentials=pika.PlainCredentials("user", "PASSWORD")))
         
     channel = connection.channel()
