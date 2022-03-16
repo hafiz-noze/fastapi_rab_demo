a5fb73460d906bfbf98a03048073df268d474b3f
Hafizur Rahman <hafizur.rahman@STS.local>
2022-03-15 23:20:32 -0400

    sdfasdf

--- producer/app.py
+++ producer/app.py
@@ -19,7 +19,7 @@ def post_message(message: str):
     channel = connection.channel()
 
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hello', durable=True)
 
     channel.basic_publish(exchange='', routing_key='hello', body=message)
 
